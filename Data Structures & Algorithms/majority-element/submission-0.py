79a4bf580c0742d3a55535647e25b35bc243fa75
Erande Siddhant Suresh <siddhantsirjese999@gmail.com>
2026-05-30 22:38:28 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash_set = {}
+        for num in nums:
+            if num in hash_set:
+                hash_set[num] += 1
+            else:
+                hash_set[num] = 1
+        top_k = sorted(hash_set, key = hash_set.get)[::-1]
+        return top_k[0]
